dcb7bc5ccb8028d8da0b17b960dd41acfd95080d
Konou Vang <konouvang@Konous-MBP.attlocal.net>
2019-04-25 19:27:07 -0500

    code is corrected

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-$( document ).ready( readyNow );
+$(document).ready(readyNow);
 
 const salary = 0;
 let employees = [];
 
-function addEmployees(){
-  console.log( 'in addEmployees' );
+function addEmployees() {
+  console.log('in addEmployees');
   // get user input create a new object
   let newEmployees = {
-    firstName: $( '#firstNameIn' ).val(),
-    lastName: $( '#lastNameIn').val(),
-    id: $( '#idIn' ).val(),
-    title: $( '#titleIn').val(),
-    annualSalary: $( '#salaryIn').val()
+    firstName: $('#firstNameIn').val(),
+    lastName: $('#lastNameIn').val(),
+    id: $('#idIn').val(),
+    title: $('#titleIn').val(),
+    // parseInt converts a string into a number. The line below WITHOUT parseInt, would be a string
+    annualSalary: parseInt($('#salaryIn').val())
   }
   // push the new employees in array
-  employees.push( newEmployees );
+  employees.push(newEmployees);
   // empty inputs
-  $( '#firstNameIn' ).val( '' );
-  $( '#lastNameIn' ).val( '' );
+  $('#firstNameIn').val('');
+  $('#lastNameIn').val('');
+  $('#idIn').val('');
+  $('#titleIn').val('');
+  $('#salaryIn').val('');
   // calculate totalMonthly
   calculateTotalMonthly();
   // update DOM
@@ -26,43 +30,39 @@ function addEmployees(){
 
 
 
-function calculateTotalMonthly(){
+function calculateTotalMonthly() {
   // console.log( 'in calculateTotalMonthly' );
   // loop through employees Array
-  let totalSalary = 0   ;
-  for( let i = 0; i < employees.length; i++ ){
+  let totalSalary = 0;
+  for (let i = 0; i < employees.length; i++) {
     // got each employees, add up total of all salary
-    totalSalary = totalSalary + employees[ i ].annualSalary;
-    return totalSalary
-  } // end for
-  //console.log( 'totalSalary:', totalSalary );
-  // add totalSalary from the salary for totalmonthlysalary
-  let el = 0
-  el.empty();
-  el.append ( totalSalary );
+    totalSalary = totalSalary + employees[i].annualSalary;
+  }
+
+  $('#calulateTotalMonthly').text(totalSalary);
 } // end calculateTotalMonthly
 
-function displayHire() {
-  console.log( 'in displayHire');
+function displayEmployees() {
+  console.log('in displayEmployees');
   // target output by ID
-  let el = $( '#hireOut' );
+  let el = $('#hireOut');
   // empty
   el.empty();
   // loop through employees array
-  for( person of hire ){
+  for (let person of employees) {
     // for each purchase, create a list item
-    el.append( `<li>` + person.firstName + `: ` + person.lastName + `: ` + person.id + `: ` + person.title +`: `+ person.annualSalary +`</li>` );
+    el.append(`<li>` + person.firstName + `: ` + person.lastName + `: ` + person.id + `: ` + person.title + `: ` + person.annualSalary + `</li>`);
   } // end for
 } // end displayPurchases
 
 function readyNow() {
   // display salary
   // target salaryOut by id
-  let el = $( '#salaryOut' );
+  let el = $('#salaryOut');
   el.empty();
-  el.append( salary );
+  el.append(salary);
   // handle click event
-  $( '#addEmployeesButton' ).on( 'click', addEmployees );
+  $('#addEmployeesButton').on('click', addEmployees);
   //init display
   calculateTotalMonthly();
 } // end readyNow
